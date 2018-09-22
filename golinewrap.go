ecf911ca0ef66fae51383a534e0c600027294d2d
Karrick S. McDermott <karrick@karrick.net>
2018-09-21 20:06:40 -0400

    everything but Write

--- golinewrap.go
+++ golinewrap.go
@@ -4,122 +4,209 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"os"
+	"strings"
 	"unicode/utf8"
 )
+
+func debug(format string, a ...interface{}) (int, error) {
+	if true {
+		return 0, nil
+	}
+	return fmt.Fprintf(os.Stderr, format, a...)
+}
 
 // Writer is a structure that writes to the underlying io.Writer, but forces
 // line wrapping at the specified width.
 type Writer struct {
 	io.Writer
-	bb           *bytes.Buffer
-	width        int
-	prefixLength int
-	prefix       []byte
+	lb            *bytes.Buffer
+	max           int // max number of columns to fill for each line
+	remaining     int // remaining columns in the line buffer
+	prefixColumns int // number of columns used by prefix
+	prefix        string
 }
 
 // New returns a new Writer using the specified width and prefix string for each
 // line.
 func New(w io.Writer, width int, prefix string) (*Writer, error) {
-	prefixBytes := []byte(prefix)
-	prefixLength := len([]rune(prefix))
+	prefixColumns := utf8.RuneCountInString(prefix)
 
-	if width <= 0 || width <= prefixLength {
-		return nil, fmt.Errorf("cannot create Writer unless width (%d) is greater than zero and greater than length of prefix: %d.", width, prefixLength)
+	if width <= 0 || width <= prefixColumns {
+		return nil, fmt.Errorf("cannot create Writer unless width (%d) is greater than zero and greater than number of columns used by prefix: %d.", width, prefixColumns)
 	}
+
+	// NOTE: The line buffer is sized for single byte runes, and will be
+	// extended as required when runes that require more than a single byte are
+	// emitted.
 
 	ww := &Writer{
-		Writer:       w,
-		bb:           bytes.NewBuffer(make([]byte, 0, width)),
-		width:        width,
-		prefixLength: prefixLength,
+		Writer:        w,
+		lb:            bytes.NewBuffer(make([]byte, 0, width+1)),
+		max:           width,
+		prefixColumns: prefixColumns,
 	}
 
-	if prefixLength > 0 {
-		ww.prefix = prefixBytes
+	if prefixColumns > 0 {
+		_, err := ww.lb.WriteString(prefix)
+		if err != nil {
+			return nil, err
+		}
+		ww.prefix = prefix
+		ww.remaining = ww.max - ww.prefixColumns
 	}
 
 	return ww, nil
 }
 
-// Write writes the contents of buf to the underlying io.Writer, wrapping lines
-// as necessary to prevent line lengths from exceeding the pre-configured
-// width. Each pilcrow rune, ¶, in the byte sequence causes a new paragraph to
-// be emitted.
-func (ww *Writer) Write(buf []byte) (int, error) {
-	// When there is nothing to write, we still want to invoke the underlying
-	// io.Writer with the empty buffer.
-	if len(buf) == 0 {
-		return ww.Writer.Write(buf)
+// flush flushes the contents of line buffer to underlying Writer. This method
+// is called at the conclusion of every public method, not necessarily for each
+// line.
+func (ww *Writer) flush() (int, error) {
+	debug("flush: %q\n", string(ww.lb.Bytes()))
+	if ww.lb.Len() == 0 {
+		return 0, nil
+	}
+	nw, err := ww.lb.WriteTo(ww.Writer)
+	return int(nw), err
+}
+
+// newline appends newline to line buffer then flushes to underlying writer
+// because this library is line based.
+func (ww *Writer) newline() (int, error) {
+	debug("newline\n")
+
+	if _, err := ww.lb.WriteRune('\n'); err != nil {
+		return 0, err
 	}
 
+	// After newline written, the entire line length is available.
+	ww.remaining = ww.max
+
+	// Because this library is meant to be line based, go ahead and flush the
+	// contents of the line buffer after each newline.
+	nw, err := ww.flush()
+	if err != nil {
+		return nw, err
+	}
+
+	return nw, ww.writePrefix()
+}
+
+func (ww *Writer) writePrefix() error {
+	debug("write prefix\n")
+
+	if ww.prefixColumns == 0 {
+		return nil
+	}
+
+	ww.remaining -= ww.prefixColumns
+	_, err := ww.lb.WriteString(ww.prefix)
+
+	return err
+}
+
+// WriteParagraph writes p to the underlying io.Writer, wrapping lines as
+// necessary to prevent line lengths from exceeding the pre-configured width.
+func (ww *Writer) WriteParagraph(p string) (int, error) {
+	debug("WriteParagraph(%q): %q; %d\n", p, string(ww.lb.Bytes()), ww.remaining)
+
+	var tw int // total written
+
+	for _, word := range strings.Fields(p) {
+		nw, err := ww.writeWord(word)
+		tw += nw
+		if err != nil {
+			return tw, err
+		}
+	}
+
+	// All words for this paragraph have been written. Write newline to buffer
+	// and flush.
+	debug("# paragraph complete; line buffer: %q\n", string(ww.lb.Bytes()))
+
+	nw, err := ww.newline()
+	tw += nw
+
+	// Do not need to flush again after newline, because we do not want the next
+	// prefix to be flushed yet.
+	return nw, err
+}
+
+// WriteWord writes w to the underlying io.Writer, wrapping lines as necessary
+// to prevent line lengths from exceeding the pre-configured width.
+func (ww *Writer) WriteWord(w string) (int, error) {
+	tw, err := ww.writeWord(w)
+	if err != nil {
+		return tw, err
+	}
+
+	nw, err2 := ww.flush()
+	tw += nw
+
+	if err == nil {
+		err = err2
+	}
+
+	return tw, err
+}
+
+func (ww *Writer) writeWord(w string) (int, error) {
 	var err error
 	var tw int // total written
 
-	// Need to output a paragraph for each pilcrow. Recall that a pilcrow rune
-	// requires more than a single byte, and bytes.Split wants a slice of bytes
-	// as the delimiter.
-	paragraphs := bytes.Split(buf, []byte("¶"))
+	// Number of columns this word occupies, plus a column for the final space
+	// or newline.
+	rc := utf8.RuneCountInString(w)
 
-	for _, paragraph := range paragraphs {
-		// When there is a prefix, and line buffer is empty, append the prefix
-		// to the line buffer.
-		if ww.prefixLength > 0 && ww.bb.Len() == 0 {
-			if _, err = ww.bb.Write(ww.prefix); err != nil {
-				return tw, err
-			}
-		}
+	debug("WriteWord(%q); %q; %d\n", w, string(ww.lb.Bytes()), ww.remaining)
 
-		words := bytes.Fields(paragraph)
-
-		for _, word := range words {
-			// When there is not enough room in the line buffer to append this
-			// word followed by a space rune, then write out line buffer
-			// followed by a newline character.
-			if ww.bb.Len()+utf8.RuneCount(word)+1 >= ww.width {
-				// Maybe the line buffer is empty, and this word is too long to
-				// fit on a line. Only flush line buffer if it is not empty.
-				if ww.bb.Len() > 0 {
-					// Change final space to newline rune.
-					b := ww.bb.Bytes()
-					b[len(b)-1] = '\n'
-
-					nw, err := ww.bb.WriteTo(ww.Writer)
-					tw += int(nw)
-					if err != nil {
-						return tw, err
-					}
-
-					ww.bb.Reset()
-
-					if ww.prefixLength > 0 {
-						if _, err = ww.bb.Write(ww.prefix); err != nil {
-							return tw, err
-						}
-					}
-				}
-			}
-
-			// The line buffer is ready for appending this word followed by a
-			// space rune.
-			if _, err = ww.bb.Write(append(word, ' ')); err != nil {
-				return tw, err
-			}
-		}
-		// POST: All words for this paragraph written into bb.
-
-		// Change final space to newline rune.
-		if ww.bb.Len() > 0 {
-			b := ww.bb.Bytes()
-			b[len(b)-1] = '\n'
-
-			nw, err := ww.bb.WriteTo(ww.Writer)
-			tw += int(nw)
-			if err != nil {
-				return tw, err
-			}
-			ww.bb.Reset()
+	if ww.remaining < rc {
+		// Not enough room for w and a newline.
+		if tw, err = ww.newline(); err != nil {
+			return tw, err
 		}
 	}
 
-	return tw, nil
+	if ww.max-ww.remaining != ww.prefixColumns {
+		debug("not the first word on line\n")
+		// Write a space before this word if it's not the first thing after the
+		// prefix.
+		if _, err = ww.lb.WriteRune(' '); err != nil {
+			return tw, err
+		}
+		ww.remaining--
+	}
+
+	if _, err = ww.lb.WriteString(w); err != nil {
+		return tw, err
+	}
+	ww.remaining -= rc
+
+	return tw, err
+}
+
+// WriteRune writes r to the underlying io.Writer, wrapping lines as necessary
+// to prevent line lengths from exceeding the pre-configured width.
+func (ww *Writer) WriteRune(r rune) (int, error) {
+	var err error
+	var tw int
+
+	debug("WriteRune(%q): %q; %d\n", r, string(ww.lb.Bytes()), ww.remaining)
+
+	if ww.remaining < 2 {
+		// Not enough room for r and a newline.
+		if tw, err = ww.newline(); err != nil {
+			return tw, err
+		}
+	}
+
+	if _, err := ww.lb.WriteRune(r); err != nil {
+		return tw, err
+	}
+	ww.remaining--
+
+	nw, err := ww.flush()
+	tw += nw
+	return tw, err
 }
---
+++ golinewrap_test.go
@@ -0,0 +1,213 @@
+package golinewrap_test
+
+import (
+	"bytes"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/karrick/golinewrap"
+)
+
+func TestNewReturnsErrorWhenPrefixTooLong(t *testing.T) {
+	prefix := "1234567890"
+	width := len(prefix)
+	_, err := golinewrap.New(os.Stderr, width, prefix)
+	if want := "columns"; err == nil || !strings.Contains(err.Error(), want) {
+		t.Errorf("GOT: %v; WANT: %v", err, want)
+	}
+}
+
+func TestWriteRune(t *testing.T) {
+	emit := func(t *testing.T, width int, prefix string, rr []rune) string {
+		bb := new(bytes.Buffer)
+
+		lw, err := golinewrap.New(bb, width, prefix)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		for _, r := range rr {
+			_, err = lw.WriteRune(r)
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+
+		return string(bb.Bytes())
+	}
+
+	t.Run("less than one line", func(t *testing.T) {
+		got := emit(t, 5, ">", []rune{'1', '2'})
+		if want := ">12"; got != want {
+			t.Errorf("GOT: %q; WANT: %q", got, want)
+		}
+	})
+
+	t.Run("exactly one line", func(t *testing.T) {
+		got := emit(t, 5, ">", []rune{'1', '2', '3'})
+		if want := ">123"; got != want {
+			t.Errorf("GOT: %q; WANT: %q", got, want)
+		}
+	})
+
+	t.Run("between first and second line", func(t *testing.T) {
+		got := emit(t, 5, ">", []rune{'1', '2', '3', '4'})
+		if want := ">123\n>4"; got != want {
+			t.Errorf("GOT: %q; WANT: %q", got, want)
+		}
+	})
+
+	t.Run("exactly two lines", func(t *testing.T) {
+		got := emit(t, 5, ">", []rune{'1', '2', '3', '4', '5', '6'})
+		if want := ">123\n>456"; got != want {
+			t.Errorf("GOT: %q; WANT: %q", got, want)
+		}
+	})
+}
+
+func TestWriteWord(t *testing.T) {
+	emit := func(t *testing.T, width int, prefix string, ww []string) string {
+		bb := new(bytes.Buffer)
+
+		lw, err := golinewrap.New(bb, width, prefix)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		for _, w := range ww {
+			_, err = lw.WriteWord(w)
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+
+		return string(bb.Bytes())
+	}
+
+	t.Run("less than one line", func(t *testing.T) {
+		got := emit(t, 10, ">", []string{"one"})
+		if want := ">one"; got != want {
+			t.Errorf("GOT: %q; WANT: %q", got, want)
+		}
+	})
+
+	t.Run("exactly one line", func(t *testing.T) {
+		t.Run("one word", func(t *testing.T) {
+			got := emit(t, 9, ">", []string{"exactly"})
+			if want := ">exactly"; got != want {
+				t.Errorf("GOT: %q; WANT: %q", got, want)
+			}
+		})
+
+		t.Run("two words", func(t *testing.T) {
+			got := emit(t, 9, ">", []string{"one", "two"})
+			if want := ">one two"; got != want {
+				t.Errorf("GOT: %q; WANT: %q", got, want)
+			}
+		})
+	})
+
+	t.Run("between one and two lines", func(t *testing.T) {
+		got := emit(t, 9, ">", []string{"another", "test"})
+		if want := ">another\n>test"; got != want {
+			t.Errorf("GOT: %q; WANT: %q", got, want)
+		}
+	})
+
+	t.Run("exactly two lines", func(t *testing.T) {
+		got := emit(t, 9, ">", []string{"another", "another"})
+		if want := ">another\n>another"; got != want {
+			t.Errorf("GOT: %q; WANT: %q", got, want)
+		}
+	})
+
+	t.Run("between two and three lines", func(t *testing.T) {
+		got := emit(t, 9, ">", []string{"another", "another", "another"})
+		if want := ">another\n>another\n>another"; got != want {
+			t.Errorf("GOT: %q; WANT: %q", got, want)
+		}
+	})
+}
+
+func TestWriteParagraph(t *testing.T) {
+	emit := func(t *testing.T, width int, prefix string, p string) string {
+		bb := new(bytes.Buffer)
+
+		lw, err := golinewrap.New(bb, width, prefix)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		_, err = lw.WriteParagraph(p)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		return string(bb.Bytes())
+	}
+
+	t.Run("without trailing newline", func(t *testing.T) {
+		t.Run("less than one line", func(t *testing.T) {
+			got := emit(t, 15, ">", "one two three")
+			if want := ">one two three\n"; got != want {
+				t.Errorf("GOT: %q; WANT: %q", got, want)
+			}
+		})
+
+		t.Run("exactly one line", func(t *testing.T) {
+			got := emit(t, 14, ">", "one two three")
+			if want := ">one two three\n"; got != want {
+				t.Errorf("GOT: %q; WANT: %q", got, want)
+			}
+		})
+
+		t.Run("between one and two lines", func(t *testing.T) {
+			got := emit(t, 13, ">", "one two three")
+			if want := ">one two three\n"; got != want {
+				t.Errorf("GOT: %q; WANT: %q", got, want)
+			}
+		})
+
+		t.Run("exactly two lines", func(t *testing.T) {
+			got := emit(t, 4, ">", "one two")
+			if want := ">one\n>two\n"; got != want {
+				t.Errorf("GOT: %q; WANT: %q", got, want)
+			}
+		})
+	})
+
+	t.Run("with trailing newline", func(t *testing.T) {
+		t.Run("less than one line", func(t *testing.T) {
+			got := emit(t, 15, ">", "one two three\n")
+			if want := ">one two three\n"; got != want {
+				t.Errorf("GOT: %q; WANT: %q", got, want)
+			}
+		})
+
+		t.Run("exactly one line", func(t *testing.T) {
+			got := emit(t, 14, ">", "one two three\n")
+			if want := ">one two three\n"; got != want {
+				t.Errorf("GOT: %q; WANT: %q", got, want)
+			}
+		})
+
+		t.Run("between one and two lines", func(t *testing.T) {
+			got := emit(t, 13, ">", "one two three\n")
+			if want := ">one two three\n"; got != want {
+				t.Errorf("GOT: %q; WANT: %q", got, want)
+			}
+		})
+
+		t.Run("exactly two lines", func(t *testing.T) {
+			got := emit(t, 4, ">", "one two\n")
+			if want := ">one\n>two\n"; got != want {
+				t.Errorf("GOT: %q; WANT: %q", got, want)
+			}
+		})
+	})
+}
+
+// func TestWrite(t *testing.T) {
+// 	t.Skip("TODO: passes buffer directly to underlying writer? or to line buffer?")
+// }
